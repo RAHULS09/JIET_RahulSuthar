af76f8c16d77df2341b4a7930cd0fc4746ccadd4
Rahul Suthar <suthar.rahul009@gmail.com>
2021-06-20 12:39:45 +0530

    assignment 6

--- assignment6/scripts/script.js
+++ assignment6/scripts/script.js
@@ -1,5 +1,5 @@
 let lightTheme = "styles/light.css";
-let darkTheme = "styles/dark.css";
+
 
 // Clears the screen on click of C button.
 function clearScreen() {
@@ -12,16 +12,4 @@ function liveScreen(value) {
     res.value = "";
   }
   res.value += value;
-}
-// Swaps the style sheet in order to  achieve dark mode.
-function changeTheme() {
-  let darkMode = document.getElementById("dark-mode");
-  let theme = document.getElementById("theme");
-  if (theme.getAttribute("href") == lightTheme) {
-    theme.href = darkTheme;
-    darkMode.innerHTML = "Light Mode 🌞";
-  } else {
-    theme.href = lightTheme;
-    darkMode.innerHTML = "Dark Mode 🌙";
-  }
 }
